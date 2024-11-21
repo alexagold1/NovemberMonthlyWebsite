bd30ca65f1824b5a43fb707af0ced1d91feda6c2
alexagold1 <526agold@frhsd.com>
2024-11-21 09:31:34 -0500

    finished javascipt and styled my text tags

--- script.js
+++ script.js
@@ -12,19 +12,19 @@ console.log(userName)
 
 let myStory = `
 
-<p> Hello ${userName}. You will be going up against the all mighty ${userVillain}. Your advantage over them is that you have ${userSuperpower} and that you have your ${userAdjective} sidekick ${userFriendName}. </p>
+<p> Hello <b>${userName}</b>. You will be going up against the all mighty <b>${userVillain}</b>. Your advantage over them is that you have <b>${userSuperpower}</b> and that you have your <b>${userAdjective}</b> sidekick <b>${userFriendName}.</b> </p>
 
-<p>Together, ${userName} and ${userFriendName} save the city of ${userCity}. ${userVillain} is looking to take over the ${userQuest} at ${userTime}, you and ${userFriendName} will leave at ${userTimeSecond} to head to the street by ${userQuest} </p> 
+<p>Together, <b>${userName}</b> and <b>${userFriendName}</b> save the city of <b>${userCity}</b>. <b>${userVillain}</b> is looking to take over the <b>${userQuest}</b> at <b>${userTime}</b>, you and <b>${userFriendName}</b> will leave at <b>${userTimeSecond}</b> to head to the street by <b>${userQuest}</b> </p> 
 
-<p> Your mode of transportation will be in a ${userVehicle} which will help you both get there quicker. You and ${userFriendName} get to the ${userQuest} before ${userVillain} and keep close look out to see when he arrives. </p>
+<p> Your mode of transportation will be in a <b>${userVehicle}</b> which will help you both get there quicker. You and <b>${userFriendName}</b> get to the <b>${userQuest}</b> before <b>${userVillain}</b> and keep close look out to see when he arrives. </p>
 
-<p> Eventually, ${userVillain} arrives at the ${userQuest} but before ${userVillain} can do anything, you and ${userFriendName} spring into action to help protect the ${userQuest} and more importantly, the ${userCity}. </p>
+<p> Eventually, <b>${userVillain}</b> arrives at the <b>${userQuest}</b> but before <b>${userVillain}</b> can do anything, you and <b>${userFriendName}</b> spring into action to help protect the <b>${userQuest}</b> and more importantly, the <b>${userCity}</b>. </p>
 
-<p> In the end, you and ${userFriendName} take down ${userVillain} by suprising him and using your ${userSuperpower} to get him on the ground and sucessfully capture ${userVillain}. The police arrive on scene and put ${userVillain} in handcuffs. </p>
+<p> In the end, you and <b>${userFriendName}</b> take down <b>${userVillain}</b> by suprising him and using your <b>${userSuperpower}</b> to get him on the ground and sucessfully capture <b>${userVillain}</b>. The police arrive on scene and put <b>${userVillain}</b> in handcuffs. </p>
 
-<p> Congrats on saving the day! You and ${userFriendName} are known all around ${userCity} once again and recieve praise by the civilians for your work! 
+<p> Congrats on saving the day! You and <b>${userFriendName}</b> are known all around <b>${userCity}</b> once again and recieve praise by the civilians for your work! 
 <p> `
 
 console.log(myStory)
 
-document.getElementById('story').innerHTML = myStory
+document.getElementById('story').innerHTML = myStory 
